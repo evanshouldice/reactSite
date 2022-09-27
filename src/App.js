c38c114c9d64e743cae099dc5fd0ddbdf7a4ddd2
dicect <65144276+dicect@users.noreply.github.com>
2022-09-27 16:03:10 -0400

    got update functionality in modal working

--- src/App.js
+++ src/App.js
@@ -8,37 +8,54 @@ function App() {
   const [employees, setEmployees] = useState(
     [
       {
+        id: 1,
         name: "Evan", 
         role: "Developer", 
         img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?cs=srgb&dl=pexels-laura-stanley-1751542.jpg&fm=jpg"
       },
       {
+        id: 2,
         name: "Bob", 
         role: "Manager", 
         img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       },
       {
+        id: 3,
         name: "Andrea", 
         role: "Developer", 
         img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       },
       {
+        id: 4,
         name: "Justin", 
         role: "UI Designer", 
         img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       },
       {
+        id: 5,
         name: "Annie", 
         role: "Front-end Developer", 
         img: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       },
       {
+        id: 6,
         name: "Hannah", 
         role: "Project Manager", 
         img: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       }
     ]
   );
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee) => {
+      if(id == employee.id){
+        return{...employee, name: newName, role: newRole};
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -53,13 +70,14 @@ function App() {
         />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              console.log(employee);
               return (
                 <Employee 
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
               
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
-  const [show, setShow] = useState(false);
+function EditEmployee(props) {
+    const [name, setName] = useState(props.name);
+    const [role, setRole] = useState(props.role);
+    
+    const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  return (
+    return (
     <>
         <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
             Update
@@ -24,7 +27,15 @@ function EditEmployee() {
           <Modal.Title>Modal title</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form id='editModal' className="w-full max-w-sm">
+        <form 
+            onSubmit={(e) => {
+                e.preventDefault();
+                console.log('hello from edit employee');
+                props.updateEmployee(props.id, name, role);
+            }}
+            id='editModal' 
+            className="w-full max-w-sm"
+        >
             <div className="md:flex md:items-center mb-6">
                 <div className="md:w-1/3">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
@@ -32,7 +43,15 @@ function EditEmployee() {
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" value="Jane Doe" />
+                    <input 
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                        id="name" 
+                        type="text" 
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -42,7 +61,15 @@ function EditEmployee() {
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="role" type="text" value="Developer" />
+                    <input 
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                        id="role" 
+                        type="text" 
+                        value={role}
+                        onChange={(e) => {
+                            setRole(e.target.value);
+                        }}
+                    />
                 </div>
             </div>
         </form>
