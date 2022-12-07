d8a1ec466eb9a9ca8607d620d4d19e6bbe3c25fa
dicect <65144276+dicect@users.noreply.github.com>
2022-12-06 22:03:13 -0500

    Added routing and adjusted header styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
-import Employees from './pages/Employee';
+import Employees from './pages/Employees';
+import Customers from './pages/Customers';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <Header>
-      <Employees/>
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path='/employees' element={<Employees/>} />
+          <Route path='/customers' element={<Customers/>} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { NavLink } from 'react-router-dom'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Employees', href: '/Employees', current: true },
+  { name: 'Customers', href: '/Customers', current: false },
+  { name: 'Projects', href: '/other', current: false },
+  { name: 'Calendar', href: '/other2', current: false },
 ]
 
 function classNames(...classes) {
@@ -16,6 +17,7 @@ function classNames(...classes) {
 
 export default function Header(props) {
   return (
+    <>
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
@@ -36,17 +38,20 @@ export default function Header(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'no-underline bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'no-underline px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
+                        to={item.href}
+                        className={({isActive}) => {
+                          return (
+                            'no-underline px-3 py-2 rounded-md text-sm font-medium text-white' + 
+                            (!isActive 
+                              ? 'text-gray-300 hover:bg-gray-700 hover:text-white' 
+                              : 'bg-gray-900 text-white')
+                            );
+                        }}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
@@ -59,57 +64,6 @@ export default function Header(props) {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
               </div>
             </div>
           </div>
@@ -117,25 +71,32 @@ export default function Header(props) {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
+                <NavLink
+                key={item.name}
+                to={item.href}
+                className={({isActive}) => {
+                  return (
+                    'block px-3 py-2 rounded-md text-base font-medium no-underline' + 
+                    (!isActive 
+                      ? 'text-gray-300 hover:bg-gray-700 hover:text-white' 
+                      : 'bg-gray-900 text-white')
+                    );
+                }}
+              >
+                
                   {item.name}
-                </Disclosure.Button>
+                </NavLink>
               ))}
             </div>
           </Disclosure.Panel>
-          {props.children}
         </>
       )}
     </Disclosure>
-
+    <div className='bg-gray-300'>
+      <div className='max-w-7xl mx-auto bg-gray-300 min-h-screen px-2 py-2'>
+        {props.children}
+      </div>
+    </div>
+    </>
   )
 }
