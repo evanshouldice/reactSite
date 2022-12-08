9551b8eaddcd0d82a42d52538bbf47d5ffd353d9
dicect <65144276+dicect@users.noreply.github.com>
2022-12-07 22:54:22 -0500

    basic useEffect practice

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import Customers from './pages/Customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dictionary from './components/Dictionary';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/employees' element={<Employees/>} />
           <Route path='/customers' element={<Customers/>} />
+          <Route path='/dictionary' element={<Dictionary/>} />
         </Routes>
       </Header>
     </BrowserRouter>
