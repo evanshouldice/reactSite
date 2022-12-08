c5a7511a9eb0e3ffa07e1946e9c22786d716781f
dicect <65144276+dicect@users.noreply.github.com>
2022-12-07 23:27:41 -0500

    added dictionary and definition pages, started definition api

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import Customers from './pages/Customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dictionary from './components/Dictionary';
+import Dictionary from './pages/Dictionary';
+import Definition from './pages/Definition';
 
 function App() {
 
@@ -16,9 +17,10 @@ function App() {
     <BrowserRouter>
       <Header>
         <Routes>
-          <Route path='/employees' element={<Employees/>} />
-          <Route path='/customers' element={<Customers/>} />
-          <Route path='/dictionary' element={<Dictionary/>} />
+          <Route path='/employees' element={<Employees />} />
+          <Route path='/customers' element={<Customers />} />
+          <Route path='/dictionary' element={<Dictionary />} />
+          <Route path='/definition' element={<Definition />} />
         </Routes>
       </Header>
     </BrowserRouter>
